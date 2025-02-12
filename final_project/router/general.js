4759fe59222cbeb7f280572e56b1b0c5f6bb95d3
pechabar <pechabar@cvut.fel.cz>
2025-02-12 23:23:19 +0100

    small improvements

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,9 +25,10 @@ public_users.post("/register", (req,res) => {
 
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+public_users.get('/', function (req, res) {
+    return res.status(200).send(JSON.stringify(books, null, 2)); // Pretty-print JSON
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
